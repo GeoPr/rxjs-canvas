a77e2df801ccd7cf609ea10c9fba9177c91366f9
George <klassnoyparen@gmail.com>
2020-12-02 08:46:24 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-// import { fromEvent } from 'rxjs'
-// import {
-//   map,
-//   switchMap,
-//   takeUntil,
-//   pairwise,
-//   withLatestFrom,
-//   startWith,
-// } from 'rxjs/operators'
+import { fromEvent } from 'rxjs'
+import {
+  switchMap,
+  map,
+  pairwise,
+  takeUntil,
+  withLatestFrom,
+  startWith,
+} from 'rxjs/operators'
 
 const canvas = document.getElementById('canvas')
 const button = document.getElementById('button')
@@ -20,66 +20,6 @@ const scale = window.devicePixelRatio
 canvas.width = rect.width * scale
 canvas.height = rect.height * scale
 ctx.scale(scale, scale)
-
-// const mouseMove$ = fromEvent(canvas, 'mousemove')
-// const mouseDown$ = fromEvent(canvas, 'mousedown')
-// const mouseUp$ = fromEvent(canvas, 'mouseup')
-// const mouseOut$ = fromEvent(canvas, 'mouseout')
-
-// const createInputStream = inp => {
-//   return fromEvent(inp, 'input').pipe(
-//     map(e => e.target.value),
-//     startWith(inp.value),
-//   )
-// }
-
-// const lineWidth$ = createInputStream(range)
-// const color$ = createInputStream(color)
-
-// const stream$ = mouseDown$.pipe(
-//   withLatestFrom(lineWidth$, color$, (_, lineWidth, color) => ({
-//     // _ - mouseDown$
-//     lineWidth,
-//     color,
-//   })),
-//   switchMap(options => {
-//     return mouseMove$.pipe(
-//       map(e => ({ x: e.offsetX, y: e.offsetY, options })),
-//       pairwise(),
-//       takeUntil(mouseUp$),
-//       takeUntil(mouseOut$),
-//     )
-//   }),
-// )
-
-// stream$.subscribe(([from, to]) => {
-//   const { lineWidth, color } = from.options
-
-//   ctx.strokeStyle = color
-//   ctx.lineWidth = lineWidth
-
-//   ctx.beginPath()
-
-//   ctx.moveTo(from.x, from.y)
-//   ctx.lineTo(to.x, to.y)
-//   ctx.stroke()
-
-//   ctx.closePath()
-// })
-
-// fromEvent(button, 'click').subscribe(() => {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height)
-// })
-
-import { fromEvent } from 'rxjs'
-import {
-  switchMap,
-  map,
-  pairwise,
-  takeUntil,
-  withLatestFrom,
-  startWith,
-} from 'rxjs/operators'
 
 const mouseMove$ = fromEvent(canvas, 'mousemove')
 const mouseDown$ = fromEvent(canvas, 'mousedown')
